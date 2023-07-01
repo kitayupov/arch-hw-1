f3a68b212cbd84bc42d2bbc9fc345223b3940254
Kamil Tayupov <k.i.tayupov@mail.ru>
2023-07-01 20:10:55 +0300

    check if d is more than zero

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,3 +1,5 @@
+import kotlin.math.sqrt
+
 /**
  * Returns the solution of the quadratic equation
  * a * x^2 + b * x  + c = 0
@@ -5,5 +7,10 @@
 fun solve(a: Double, b: Double, c: Double, e: Double): Array<Double> {
     val d = b * b - 4 * a * c
     if (d + e < 0) return emptyArray<Double>()
+    if (d > e) {
+        val x1 = (-b - sqrt(d)) / (2 * a)
+        val x2 = (-b + sqrt(d)) / (2 * a)
+        return arrayOf(x1, x2)
+    }
     return emptyArray<Double>()
 }
--- src/test/kotlin/FunctionsKtTest.kt
+++ src/test/kotlin/FunctionsKtTest.kt
@@ -1,11 +1,21 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
+
+private const val epsilon = 1e-7
 
 class FunctionsKtTest {
 
     @Test
-    fun `should return empty roots`() {
-        val result = solve(1.0, 0.0, 1.0, 1e-7)
+    fun `should return no roots`() {
+        val result = solve(1.0, 0.0, 1.0, epsilon)
         assertEquals(result.size, 0)
+    }
+
+    @Test
+    fun `should return two different roots`() {
+        val result = solve(1.0, 0.0, -1.0, epsilon)
+        assertEquals(abs(result[0] + 1) < epsilon, true)
+        assertEquals(abs(result[1] - 1) < epsilon, true)
     }
 }
